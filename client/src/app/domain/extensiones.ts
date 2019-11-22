dde46a153e24f480ff8da5639fd7ccaf49bc697e
Andres Suarez <andres27059934@gmail.com>
2019-11-22 14:38:36 -0300

    Implementado sort alfabetico y añadidas más extensiones

--- client/src/app/domain/extensiones.ts
+++ client/src/app/domain/extensiones.ts
@@ -1,1 +1,2 @@
-export const extensionesPosibles = ['pdf', 'doc', 'txt']
+const extensionesPosiblesDesordenadas = ['pdf', 'doc', 'txt', 'xls', 'xlsx', 'xlsxm', 'xlsb', 'epub', 'csv', 'mobi', 'rtf', 'html',]
+export const extensionesPosibles = extensionesPosiblesDesordenadas.sort((a, b) => a.localeCompare(b))
